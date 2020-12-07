1ea328e5e3933670ddd997104b3e2d002684a4db
Mina <dlscarmina@gmail.com>
2020-12-06 17:13:51 -0800

    Started working on landing page

---
+++ src/yelp-analyzer/src/components/ShowSearchLanding.js
@@ -0,0 +1,56 @@
+// LANDING PAGE WITH SEARCH BAR
+
+import React, { Component } from 'react';
+import '../App.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import ReviewCard from './ReviewCard';
+
+class ShowSearchLanding extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:8082/api/reviews')
+      .then(res => {
+        this.setState({
+          reviews: res.data
+        })
+      })
+      .catch(err =>{
+        console.log('Error from ShowSearchLanding');
+      })
+  };
+
+
+  render() {
+
+    return (
+      <div className="ShowSearchLanding">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <br />
+              <h2 className="display-4 text-center">Reviews List</h2>
+            </div>
+
+            <div className="col-md-11">
+              
+              <br />
+              <br />
+              <hr />
+            </div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShowReviewList;
