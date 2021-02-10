f034b5f5918e2257648cdeb6323bec064f4a741a
andrewrchen05 <30973083+andrewrchen05@users.noreply.github.com>
2021-02-10 09:24:14 -0800

    Prepare nlp.py as helper function

--- gcloud/nlp.py
+++ gcloud/nlp.py
@@ -26,19 +26,20 @@ def sample_analyze_sentiment(text_content):
 
     response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
     # Get overall sentiment of the input document
-    print(u"Document sentiment score: {}".format(response.document_sentiment.score))
-    print(
-        u"Document sentiment magnitude: {}".format(
-            response.document_sentiment.magnitude
-        )
-    )
+    # print(u"Document sentiment score: {}".format(response.document_sentiment.score))
+    return response.document_sentiment.score
+    #print(
+    #    u"Document sentiment magnitude: {}".format(
+    #        response.document_sentiment.magnitude
+    #    )
+    #)
     # Get sentiment for all sentences in the document
-    for sentence in response.sentences:
-        print(u"Sentence text: {}".format(sentence.text.content))
-        print(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
-        print(u"Sentence sentiment magnitude: {}".format(sentence.sentiment.magnitude))
+    # for sentence in response.sentences:
+        # print(u"Sentence text: {}".format(sentence.text.content))
+        # print(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
+        #print(u"Sentence sentiment magnitude: {}".format(sentence.sentiment.magnitude))
 
     # Get the language of the text, which will be the same as
     # the language specified in the request or, if not specified,
     # the automatically-detected language.
-    print(u"Language of the text: {}".format(response.language))
+    # print(u"Language of the text: {}".format(response.language))
