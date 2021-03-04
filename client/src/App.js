4e58a6470bc07d1c14b865a891675ec2ab3b6d38
Andrew Chen <andrewrchen05@gmail.com>
2021-03-04 15:21:21 -0800

    commenting out some navbar items

--- client/src/App.js
+++ client/src/App.js
@@ -58,30 +58,31 @@ const App = () => {
 
     return (
         <div>
-            <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <ReactBootStrap.Navbar.Brand href="#home">Restaurant Reviews with Aspect Sentiment Analysis</ReactBootStrap.Navbar.Brand>
-                <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-                    <ReactBootStrap.Nav className="mr-auto">
-                    {/* <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
-                    <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
-                    <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
-                        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
-                        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
-                        <ReactBootStrap.NavDropdown.Divider />
-                        <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
-                    </ReactBootStrap.NavDropdown> */}
-                    </ReactBootStrap.Nav>
-                    <ReactBootStrap.Nav>
-                    <ReactBootStrap.Nav.Link href="#deets">Features</ReactBootStrap.Nav.Link>
-                    <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
-                        About
-                    </ReactBootStrap.Nav.Link>
-                    </ReactBootStrap.Nav>
-                </ReactBootStrap.Navbar.Collapse>
-                </ReactBootStrap.Navbar>
             <SearchProvider config={config}>
+                <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <ReactBootStrap.Navbar.Brand href="#home">Restaurant Reviews with Aspect Sentiment Analysis</ReactBootStrap.Navbar.Brand>
+                    <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+                        <ReactBootStrap.Nav className="mr-auto">
+                        {/* <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
+                            <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
+                            <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
+                            <ReactBootStrap.NavDropdown.Divider />
+                            <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
+                        </ReactBootStrap.NavDropdown> */}
+                        </ReactBootStrap.Nav>
+                        <ReactBootStrap.Nav>
+                        <ReactBootStrap.Nav.Link href="#deets">Features</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
+                            About
+                        </ReactBootStrap.Nav.Link>
+                        </ReactBootStrap.Nav>
+                    </ReactBootStrap.Navbar.Collapse>
+                    </ReactBootStrap.Navbar>
+            
                 <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
                     {({ wasSearched }) => {
                     return (
