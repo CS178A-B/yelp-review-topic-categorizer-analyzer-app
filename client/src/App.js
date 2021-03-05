a9e52101b2bac3e5477231e1373eb041537cd85d
Mina <dlscarmina@gmail.com>
2021-03-05 14:58:45 -0800

    started routing in App.js, created Landing.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import RestaurantPage from './components/RestaurantPage.js';
+import Landing from './components/Landing.js';
 import Search from './components/Search.js';
+import RestaurantPage from './components/RestaurantPage.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 import '@elastic/eui/dist/eui_theme_dark.css';
 
 const App = () => {
 
     return (
-        <div>
+        <Router>
+            <Route path="/" component={Landing} />
+            <Route/>
+
             {/* <RestaurantPage/> */}
-            <Search/>
-        </div>
+            {/* <Landing/> */}
+            {/* <Search/> */}
+        </Router>
     );
 }
 
