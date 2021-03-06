2e4df17677735f63618363970410876515f49ab0
Andrew Chen <andrewrchen05@gmail.com>
2021-03-05 16:01:12 -0800

    star ratings on restaurant card

--- client/src/ResultView.js
+++ client/src/ResultView.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { StarFill, Star, StarHalf } from 'bootstrap-icons-react';
 import './ResultView.css'
+
+function starsRender(value) {
+  value = +value
+
+  if (value === 0.0) {
+    return ( <span>Data Not Available</span> )
+  } 
+  else if (value === 1.0) {
+    return ( <span><StarFill /><Star /><Star /><Star /><Star /></span> )
+  }
+  else if (value === 1.5) {
+    return ( <span><StarFill /><StarHalf /><Star /><Star /><Star /></span> )
+  }
+  else if (value === 2.0) {
+    return ( <span><StarFill /><StarFill /><Star /><Star /><Star /></span> )
+  }
+  else if (value === 2.5) {
+    return ( <span><StarFill /><StarFill /><StarHalf /><Star /><Star /></span> )
+  }
+  else if (value === 3.0) {
+    return ( <span><StarFill /><StarFill /><StarFill /><Star /><Star /></span> )
+  }
+  else if (value === 3.5) {
+    return ( <span><StarFill /><StarFill /><StarFill /><StarHalf /><Star /></span> )
+  }
+  else if (value === 4.0) {
+    return ( <span><StarFill /><StarFill /><StarFill /><StarFill /><Star /></span> )
+  }
+  else if (value === 4.5) {
+    return ( <span><StarFill /><StarFill /><StarFill /><StarFill /><StarHalf /></span> )
+  }
+  else if (value === 5.0) {
+    return ( <span><StarFill /><StarFill /><StarFill /><StarFill /><StarFill /></span> )
+  }
+
+}
 
 export default ({ result }) => (
   <li className="sui-result">
@@ -15,24 +52,19 @@ export default ({ result }) => (
       <ul className="sui-result__details">
         <li>
           <span className="sui-result__key">Overall Rating</span>{" "}
-          <span
-            className="sui-result__value"
-            dangerouslySetInnerHTML={{
-              __html: result.stars.snippet
-            }}
-          />
+          <span className="sui-result__value">{starsRender(result.stars.raw)}</span>
         </li>
         <li>
           <span className="sui-result__key">Food</span>{" "}
-          <span className="sui-result__value">{result.food.raw}</span>
+          <span className="sui-result__value">{starsRender(result.food.raw)}</span>
         </li>
         <li>
           <span className="sui-result__key">Ambience</span>{" "}
-          <span className="sui-result__value">{result.ambience.raw}</span>
+          <span className="sui-result__value">{starsRender(result.ambience.raw)}</span>
         </li>
         <li>
           <span className="sui-result__key">Service</span>{" "}
-          <span className="sui-result__value">{result.service.raw}</span>
+          <span className="sui-result__value">{starsRender(result.service.raw)}</span>
         </li>
       </ul>
     </div>
