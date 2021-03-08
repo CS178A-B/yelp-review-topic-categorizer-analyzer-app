cc2e71d756958b427be47302d05184e79834e5d1
andrewrchen05 <30973083+andrewrchen05@users.noreply.github.com>
2021-03-07 16:50:01 -0800

    Unlist image_url variable and add handling for NULL results

--- public/yelp_dataset/fusion.R
+++ public/yelp_dataset/fusion.R
@@ -1,21 +1,27 @@
 library(tidyverse)
 library(httr)
 
-getImageURL <- function(business_id) {
+res <- POST("https://api.yelp.com/oauth2/token",
+            body = list(grant_type = "client_credentials",
+                        client_id = client_id,
+                        client_secret = client_secret))
 
-  res <- POST("https://api.yelp.com/oauth2/token",
-              body = list(grant_type = "client_credentials",
-                          client_id = client_id,
-                          client_secret = client_secret))
-  
-  token <- content(res)$access_token
+token <- content(res)$access_token
+
+getImageURL <- function(business_id) {
   
   yelp <- "https://api.yelp.com"
   url <- modify_url(yelp, path = c("v3", "businesses", business_id))
   res <- GET(url, add_headers('Authorization' = paste("bearer", client_secret)))
   
   results <- content(res)
-  
   image_url <- results["image_url"]
-  return(image_url) 
+  
+  # print(typeof(unlist(image_url$image_url)))
+  
+  if (is.null(unlist(image_url$image_url))) {
+    return("NA")
+    } else {
+    return(unlist(image_url$image_url)) 
+  }
 }
