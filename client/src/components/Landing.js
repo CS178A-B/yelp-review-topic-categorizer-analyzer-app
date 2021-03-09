6245fa86b598eee1fac9159f66fa3b3eba3b42ca
Mina <dlscarmina@gmail.com>
2021-03-08 16:14:48 -0800

    compartmentalized landing content

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,15 +1,67 @@
 import React from 'react';
+
+import {
+    EuiPage,
+    EuiPageContent,
+    EuiEmptyPrompt,
+    EuiPageBody,
+    EuiTitle,
+    EuiSpacer,
+    EuiButton,
+  } from '@elastic/eui';
+
 import './css/Landing.css';
 import About from './About.js'
 
-const Landing = () => {
+const Landing = ({ button = <></>, content }) => {
     return (
 
             <React.Fragment>
-                <div class="landing-body">
-                    <h1 class = "landing-title">restaurant review topic categorizer & analyzer</h1>
-                </div>
-                <About/>
+                    <EuiPage paddingSize="none" class="landing-body">
+
+                        <div class="landing-feature">
+                            <EuiTitle size="l"><h1 class="landing-title">Yelp Review Topic Categorizer & Analyzer</h1></EuiTitle>
+                            <EuiSpacer/>
+
+                            <EuiPageBody paddingSize="l">
+                                <EuiPageContent
+                                verticalPosition="center"
+                                horizontalPosition="center"
+                                paddingSize="none">
+
+                                    <div class="landing-content">
+                                
+                                    <EuiEmptyPrompt
+                                        iconType="dataVisualizer"
+                                        iconColor={null}
+                                        title={<h2>You have no spice</h2>}
+                                        titleSize="xs"
+                                        body={
+                                        <React.Fragment>
+                                            <p>
+                                            Navigators use massive amounts of spice to gain a limited form of
+                                            prescience. This allows them to safely navigate interstellar space,
+                                            enabling trade and travel throughout the galaxy.
+                                            </p>
+                                            <p>You&rsquo;ll need spice to rule Arrakis, young Atreides.</p>
+                                        </React.Fragment>
+                                        }
+                                        actions={
+                                        <EuiButton size="s" color="primary" fill>
+                                            Harvest spice
+                                        </EuiButton>
+                                        }
+                                    />
+
+                                    <About/>
+                                </div>
+
+                                </EuiPageContent>
+                            </EuiPageBody>
+                        </div>
+
+                    </EuiPage>
+
             </React.Fragment>
 
     );
