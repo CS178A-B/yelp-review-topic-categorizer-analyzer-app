e3153a37da5556772d435864b5484d1ef38e2b21
Andrew Chen <andrewrchen05@gmail.com>
2021-03-09 20:54:54 -0800

    create image rendering component Image.js

---
+++ client/src/components/Image.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { EuiImage } from '@elastic/eui';
+
+export function imageLink() {
+    return(
+        <EuiImage
+        src="https://source.unsplash.com/100x100/?Nature"
+        alt="image_alt"
+        />
+    )
+}
