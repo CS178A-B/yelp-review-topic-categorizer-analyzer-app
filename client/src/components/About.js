e3f8c7290f9fda25bed1fe97c5283e160e525ec5
Giuseppi Pelayo <32854561+giuseppi@users.noreply.github.com>
2021-03-09 22:36:47 -0800

    updated 'about' tab

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -23,11 +23,10 @@ export default () => {
               The Yelp Review Topic Categorizer and Analyzer comes up with an empyrical analysis of Yelp Reviews
               and helps you choose which restaurants to eat at based on scores of different topics and helps you choose which
               restaurants to eat at based on scores of different topics. To begin using
-              this application, start by clicking 'Search' at the top. There, you can search up restaurants and receive
+              this application, start by clicking 'Search Restaurants'. There, you can search up restaurants and receive
               results based off overall rating, food, ambience and service.
           </EuiText>
           <EuiSpacer />
-
         </Fragment>
       ),
     },
@@ -36,13 +35,18 @@ export default () => {
       name: 'About The Team',
       content: (
         <Fragment>
-          <EuiSpacer />
           <EuiTitle>
             <h3>About The Team</h3>
           </EuiTitle>
+          <EuiSpacer />
           <EuiText>
-            blah blah blah
+            This application was created by Team Cool, a senior design team for CS 178B. The team consists of 4 members
+            - Andrew Chen, Timothy Koo, Mina delos Santos and Giuseppi Pelayo. All 4 members are currently seniors,
+            with Mina studying Computer Engineering while Andrew, Timothy and Giuseppi are studying Computer Science. We
+            combined our love for food, curiosity, and shared problems with Yelp to create an application that provides
+            easy-to-read scores based off different topics to easily narrow down dinning preferences.
           </EuiText>
+          <EuiSpacer />
         </Fragment>
       ),
     },
@@ -51,7 +55,7 @@ export default () => {
   return (
     <EuiTabbedContent
       tabs={tabs}
-      initialSelectedTab={tabs[1]}
+      initialSelectedTab={tabs[0]}
       autoFocus="selected"
       onTabClick={(tab) => {
         console.log('clicked tab', tab);
